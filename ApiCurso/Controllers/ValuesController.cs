cfa5985548037702ad6e06a849bc06400c7a5134
TiagoFernandesDaCosta <tiagofernandesdacosta@gmail.com>
2018-07-03 20:43:14 -0300

    atualizacao

--- ApiCurso/Controllers/ValuesController.cs
+++ ApiCurso/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ApiCurso.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", "value2", "value3","value 5" };
         }
 
         // GET api/values/5
