14901019794a4a5c64756d24bb0784ce0cc30b05
rsantosdev <p.4ever@gmail.com>
2018-07-03 20:53:00 -0300

    New machine controller

---
+++ ApiCurso/Controllers/MachineController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ApiCurso.Controllers
+{
+    [Route("api/machine")]
+    public class MachineController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult GetMachineName()
+        {
+            return Ok(new { name = Environment.MachineName });
+        }
+    }
+}
