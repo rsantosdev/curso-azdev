c2db04b616d502e1032cc0c968a6a3514a4bacb5
rsantosdev <p.4ever@gmail.com>
2018-07-03 23:21:27 -0300

    Github endpoint

---
+++ ApiCurso/Controllers/GoogleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ApiCurso.Controllers
+{
+    [Route("api/google")]
+    public class GoogleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetDotnetInfo()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var data =  await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            return Ok(data);
+        }
+    }
+}
